5b2b4426453cb327e2964460381ffa11a00f4965
tuanc62k5 <dinhtuananh.dev@gmail.com>
2026-04-21 17:42:58 +0700

    tao file gitignore

--- obj/Debug/net8.0/aznews.AssemblyInfo.cs
+++ obj/Debug/net8.0/aznews.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aznews")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc51533e36ed8f53069a3e254b3eae5b1f43bc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b370a7cbe74c8de7e209688479ef2b6ce99dce7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("aznews")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aznews")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
